87a6d91ef235789a09c23c5cdd5afe33c0bffa14
Carwi Barrios <carwelsbarrios@gmail.com>
2024-02-07 21:47:30 +0100

    comillas added

--- src/components/Leader/Leader.tsx
+++ src/components/Leader/Leader.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import styles from "./Leader.module.scss"
 import profile from "@/assets/images/profile.jpeg"
 
+import comilla1 from "@/assets/images/comilla1.png"
+import comilla2 from "@/assets/images/comilla2.png"
+
 function Leader() {
     return (
         <div className={styles.main}>
@@ -25,8 +28,26 @@ function Leader() {
                         <div className={styles.description}>
                             Soy médico especialista en Psiquiatría. Lo que más me gusta de mi profesión es la parte clínica, entender y resolver los problemas de mis pacientes. Esto es algo que compagino con mi labor como docente, en gestión e investigación
                         </div>
-                        <div className={styles.vision}>
-                            Mi visión de la salud mental es global e integradora y me gusta ayudar a la persona entendiendo sus circunstancias.
+                        <div className={styles.visionContainer}>
+                            <div className={styles.comillaWrapper}>
+                                <Image
+                                    src={comilla1}
+                                    alt="Logo"
+                                    className={styles.comillaImg}
+                                />
+                            </div>
+                            <div className={styles.visionTextContainer}>
+                                <div className={styles.visionText}>
+                                    Mi visión de la salud mental es global e integradora y me gusta ayudar a la persona entendiendo sus circunstancias.
+                                </div>
+                            </div>
+                            <div className={styles.comillaWrapper}>
+                                <Image
+                                    src={comilla2}
+                                    alt="Logo"
+                                    className={styles.comillaImg}
+                                />
+                            </div>
                         </div>
                         <div className={styles.btnContainer}>
                             <button className={styles.btn}>Saber más</button>
